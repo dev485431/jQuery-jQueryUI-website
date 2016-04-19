0c30f95228c91b4d4f4c79e078b6cea773afd157
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-04-19 16:49:53 +0200

    Improved autocomplete with on focus initialization

--- www/js/search-auto.js
+++ www/js/search-auto.js
@@ -1,11 +1,14 @@
-$(function () {
+$("#search_term").on("focus", function () {
+    // probably move all cached dom elements to the 1st script file
     var searchTerm = $("#search_term"),
         searchForm = $("#search_form"),
+        searchButton = $("#search_button"),
 
         autoCompleteUrl = 'mocks/search.json.php',
         apiVariable = 'titles',
+        acCacheName = 'autocompleteCache',
         ajaxTimeout = 10000,
-        errInputCallback = 'Please try again later';
+        errInputMessage = 'Please try again later';
 
 
     searchTerm.autocomplete({
@@ -13,26 +16,22 @@ $(function () {
         source: function (request, response) {
             var term = request.term.toLowerCase(),
                 element = this.element,
-                cache = this.element.data('autocompleteCache') || [];
+                cache = this.element.data(acCacheName) || [];
 
             if (cache.length === 0) {
-                $.ajax({
-                    url: autoCompleteUrl,
-                    dataType: "json",
-                    data: request,
-                    timeout: ajaxTimeout,
-                    success: function (data) {
+                getAutoCompleteData(request)
+                    .done(function (data) {
                         cache = data[apiVariable];
-                        element.data('autocompleteCache', cache);
+                        element.data(acCacheName, cache);
                         console.log("Doing ajax call for cache");
-                        response(search(cache, term));
-                    },
-                    error: function () {
-                        searchTerm.attr('disabled', 'disabled').val(errInputCallback).addClass('ui-state-disabled');
-                    }
-                });
+                        response(searchCache(cache, term));
+                    })
+                    .error(function () {
+                        searchTerm.prop('disabled', true).val(errInputMessage).addClass('ui-state-disabled');
+                        searchButton.prop('disabled', true);
+                    });
             } else {
-                response(search(cache, term));
+                response(searchCache(cache, term));
             }
         },
 
@@ -47,15 +46,27 @@ $(function () {
             .append("<a>" + item.label + "</a>")
             .appendTo(ul);
     };
+
+    function searchCache(cache, term) {
+        var results = [];
+        $.each(cache, function (i, val) {
+            console.log("Cache search loop");
+            if (val.toLowerCase().indexOf(term) !== -1) {
+                results.push(val);
+            }
+        });
+        return results;
+    }
+
+    function getAutoCompleteData(request) {
+        return $.ajax({
+            url: autoCompleteUrl,
+            dataType: "json",
+            data: request,
+            timeout: ajaxTimeout
+        });
+    }
 });
 
-function search(cache, term) {
-    var results = [];
-    $.each(cache, function (i, val) {
-        console.log("Cache search loop");
-        if (val.toLowerCase().indexOf(term) !== -1) {
-            results.push(val);
-        }
-    });
-    return results;
-}
+
+
