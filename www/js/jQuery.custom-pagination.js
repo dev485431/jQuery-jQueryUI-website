761543998bd31fbbcc2a76dbac93ff54d81f454f
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-04-27 17:42:16 +0200

    Custom news reader plugin - working in progress....

--- www/js/jQuery.custom-pagination.js
+++ www/js/jQuery.custom-pagination.js
@@ -1,31 +1,45 @@
 (function ($) {
 
-    var defaultItemsPerPage = 7;
+    var settings,
+        currentPage = 1;
 
+    //initial setup and binding of elements
     $.fn.customPagination = function (itemTemplate, itemsArray, options) {
-        var settings = $.extend({}, $.fn.customPagination.defaults, options);
+        settings = $.extend({}, $.fn.customPagination.defaultSettings, options);
+
         $.templates({'itemTemplate': itemTemplate});
         $.views.converters("newsdate", function (val) {
             return timeConverter(val);
         });
-        // page number here should be dynamic
-        return this.html(renderNewsHtml(getPageContent(itemsArray, 1)));
+        // save, read, set current page
+        return renderPagination(this, itemsArray, currentPage);
     };
 
+    var renderPagination = function (element, itemsArray, currentPage) {
+            element.html(renderNewsHtml(getPageData(itemsArray, currentPage)));
+        },
 
-    var renderNewsHtml = function (pageData) {
+        renderNewsHtml = function (pageData) {
             var html = '';
             for (var i = 0; i < pageData.length; i++) {
                 html += $.render.itemTemplate(pageData[i]);
             }
+            // here also append nav
+
             return html;
+
         },
 
-        getPageContent = function (itemsArray, pageNumber) {
+        renderNavigation = function (currentPage) {
+            // action listeners "on"
+            // get current page and generate back,forward accordingly
+        },
+
+        getPageData = function (itemsArray, pageNumber) {
 
             var itemsIndex = itemsArray.length - 1,
-                lastItem = defaultItemsPerPage * pageNumber - 1,
-                firstItem = defaultItemsPerPage * pageNumber - defaultItemsPerPage,
+                lastItem = settings.itemsPerPage * pageNumber - 1,
+                firstItem = settings.itemsPerPage * pageNumber - settings.itemsPerPage,
                 newsDataPerPage = [];
 
             if (lastItem <= itemsIndex) {
@@ -53,16 +67,16 @@
             var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
             var year = a.getFullYear();
             var month = months[a.getMonth()];
-            var date = a.getDate();
+            var day = a.getDate();
             var hour = a.getHours();
             var min = a.getMinutes();
             var sec = a.getSeconds();
-            var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+            var time = day + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
             return time;
         };
 
-    $.fn.customPagination.defaults = {
-        itemsPerPage: defaultItemsPerPage
+    $.fn.customPagination.defaultSettings = {
+        itemsPerPage: 7
     };
 
 }(jQuery));
