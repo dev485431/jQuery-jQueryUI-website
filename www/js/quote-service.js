c3149b737d6ca5c6571a653d8314f102f5083722
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-04-28 09:44:27 +0200

    Quotes and news loaders - default message/quote on fail

--- www/js/quote-service.js
+++ www/js/quote-service.js
@@ -5,7 +5,7 @@ QuoteService.prototype = function () {
 
     var quoteSpan = $('#lovelyquote'),
         quoteLoader = $('#quote-loader'),
-        quotesUrl = 'http://quote-service.local/api.phps',
+        quotesUrl = 'http://quote-service.local/api.php',
         defaultQuote = 'There may be no "I" in team, but there\'s a "ME" if you look hard enough. - David Brent',
         callbackParam = 'callback',
         timeout = 5000,
