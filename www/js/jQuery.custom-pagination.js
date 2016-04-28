61f128a83f91435562cc9e23e52950d998842f55
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-04-28 13:30:04 +0200

    News - oop with protype style working

--- www/js/jQuery.custom-pagination.js
+++ www/js/jQuery.custom-pagination.js
@@ -10,43 +10,48 @@
         });
 
         var customPagination = new CustomPagination(this, itemTemplate, itemsArray, settings);
-        customPagination.renderPagination();
-        return this;
+        return customPagination.renderPagination();
     };
 
 
     var CustomPagination = function (element, itemTemplate, itemsArray, settings) {
-            this.element = element;
-            this.itemTemplate = itemTemplate;
-            this.itemsArray = itemsArray;
-            this.settings = settings;
-            this.currentPage = 1;
+        this.element = element;
+        this.itemTemplate = itemTemplate;
+        this.itemsArray = itemsArray;
+        this.settings = settings;
+        this.currentPage = 1;
+    }
 
+    CustomPagination.prototype = function () {
 
-            this.previousPage = function () {
+        var previousPage = function () {
                 this.currentPage--;
-                this.renderPagination();
-            };
+                renderPagination.call(this);
+            },
 
-            this.nextPage = function () {
+            nextPage = function () {
                 this.currentPage++;
-                this.renderPagination();
-            };
+                renderPagination.call(this);
+            },
 
-            this.renderPagination = function () {
-                this.element.html(this.renderNews(this.getPageData()));
-            };
+        // setCurrentPage = function (page) {
+        //     this.currentPage = page;
+        // },
 
-            this.renderNews = function (pageData) {
+            renderPagination = function () {
+                this.element.html(renderItems.call(this, (getPageData.call(this))));
+            },
+
+            renderItems = function (pageData) {
                 var html = '';
                 for (var i = 0; i < pageData.length; i++) {
                     html += $.render.itemTemplate(pageData[i]);
                 }
-                html += this.renderNavigation();
+                html += renderNavigation.call(this);
                 return html;
-            };
+            },
 
-            this.getPageData = function () {
+            getPageData = function () {
 
                 var itemsMaxIndex = this.itemsArray.length - 1,
                     lastItem = this.settings.itemsPerPage * this.currentPage - 1,
@@ -63,9 +68,9 @@
                     }
                 }
                 return pageData;
-            };
+            },
 
-            this.renderNavigation = function () {
+            renderNavigation = function () {
                 // render html from it?
                 var nav = '<div id="pagination">' +
                     '<div id="prev"><button>Prev</button></div>' +
@@ -74,20 +79,23 @@
                 return nav;
             };
 
-        },
-
-        timeConverter = function (UNIXtimestamp) {
-            var a = new Date(UNIXtimestamp * 1000);
-            var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-            var year = a.getFullYear();
-            var month = months[a.getMonth()];
-            var day = a.getDate();
-            var hour = a.getHours();
-            var min = a.getMinutes();
-            var sec = a.getSeconds();
-            var time = day + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
-            return time;
+        return {
+            renderPagination: renderPagination
         };
+    }();
+
+    var timeConverter = function (UNIXtimestamp) {
+        var a = new Date(UNIXtimestamp * 1000);
+        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var day = a.getDate();
+        var hour = a.getHours();
+        var min = a.getMinutes();
+        var sec = a.getSeconds();
+        var time = day + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+        return time;
+    };
 
     $.fn.customPagination.defaultSettings = {
         itemsPerPage: 7
